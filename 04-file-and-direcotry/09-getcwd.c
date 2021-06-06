a6c149d2d8313af959f6840bdb407706654ec35a
haolaoshi <liujiahao.oak@gmail.com>
2021-06-06 22:22:28 +0800

    file and dirt

--- 04-file-and-direcotry/09-getcwd.c
+++ 04-file-and-direcotry/09-getcwd.c
@@ -1,1 +1,18 @@
 //
+#include <dirent.h>
+#include "00-apue.h"
+#define  EVERGREEN "/var/spool/mail"
+int main()
+{
+    char *ptr;
+    size_t size;
+
+    if(chdir(EVERGREEN) < 0)
+        err_sys(EVERGREEN);
+    ptr = malloc(PATH_MAX + 1);
+    //ptr = path_alloc(&size);
+    if(getcwd(ptr,size) == NULL)
+        err_sys("getcwd failed.");
+    printf("cwd = %s\n",ptr);
+    exit(0);
+}
